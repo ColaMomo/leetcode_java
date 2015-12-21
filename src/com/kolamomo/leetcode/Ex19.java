27ade526a8bd028a1bb8b4fa9d9863c57327b97c
jay <jayloveyun@163.com>
2015-12-21 16:50:02 +0800

    update

--- src/com/kolamomo/leetcode/Ex19.java
+++ src/com/kolamomo/leetcode/Ex19.java
@@ -1,5 +1,37 @@
 package com.kolamomo.leetcode;
 
 public class Ex19 {
+	/**
+	 * Definition for singly-linked list.
+	 * public class ListNode {
+	 *     int val;
+	 *     ListNode next;
+	 *     ListNode(int x) { val = x; }
+	 * }
+	 */
+	
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+		ListNode p = head;
+		int length = 1;
+		for(; p.next != null; p = p.next) {
+			length++;
+		}
+		p = head;
+		for(int i = 0; i < length - n - 1; i++) {
+			p = p.next;
+		}
+		if(length == n) {
+			head = head.next;
+		} else {
+			p.next = p.next.next;
+		}
+	    return head;
+	}
+
+	public class ListNode {
+		int val;
+		ListNode next;
+	 	ListNode(int x) { val = x; }
+	}
 
 }
--- src/com/kolamomo/leetcode/Ex20.java
+++ src/com/kolamomo/leetcode/Ex20.java
@@ -1,5 +1,34 @@
 package com.kolamomo.leetcode;
 
-public class Ex20 {
+import java.util.Stack;
 
+public class Ex20 {
+    public boolean isValid(String s) {
+    	Stack<Character> chQueue = new Stack<Character> ();
+        for(int i = 0; i < s.length(); i++) {
+        	char ch = s.charAt(i);
+        	if(ch == '(' || ch == '[' || ch=='{') {
+        		chQueue.push(ch);
+        	}
+        	else if(ch == ')') {
+        		if(chQueue.isEmpty() || chQueue.pop() != '(') {
+        			return false;
+        		}
+        	}  else if(ch == ']') {
+        		if(chQueue.isEmpty() || chQueue.pop() != '[') {
+        			return false;
+        		}
+        	}  else if(ch == '}') {
+        		if(chQueue.isEmpty() || chQueue.pop() != '{') {
+        			return false;
+        		}
+        	}
+        }
+        if(chQueue.isEmpty()) {
+        	return true;
+        } else {
+        	return false;
+        }
+    }
+    
 }
